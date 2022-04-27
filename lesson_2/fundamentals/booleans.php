1254c9af783bdce375204228ae983948e95839d3
Edenilson Pineda <edenilsonpineda@outlook.com>
2022-04-27 17:37:24 -0600

    add booleans example

--- lesson_2/fundamentals/booleans.php
+++ lesson_2/fundamentals/booleans.php
@@ -12,38 +12,21 @@
 <body>
     <main>
         <div id="custom-card" class="container px-4 py-5">
-            <h2 class="pb-2 border-bottom">Lesson 2 - Fundamentals (Booleans) | Tabla de verdad </h2>
+            <h2 class="pb-2 border-bottom">Lesson 2 - Fundamentals (Booleans) </h2>
             <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
                 <div class="col">
-                    <div class="table-responsive">
-                        <table class="table table-striped table-sm">
-                            <thead>
-                                <tr>
-                                    <th>p</th>
-                                    <th>q</th>
-                                    <th>P ^ Q</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                /**
-                                 * @author Edenilson Pineda
-                                 * @version 1.0
-                                 * @see https://github.com/edenilsonpineda
-                                 */
+                    <?php
+                    /**
+                     * @author Edenilson Pineda
+                     * @version 1.0
+                     * @see https://github.com/edenilsonpineda
+                     */
 
-                                $p = (boolean) TRUE;
-                                $q = (boolean) FALSE;
-                                $intersection = (boolean) FALSE;
-                                echo '<tr>';
-                                echo '<td>'.$p.'</td>';
-                                echo '<td>'.$q.'</td>';
-                                echo '<td>'.$intersection.'</td>';
-                                echo '</tr>';
-                                ?>
-                            </tbody>
-                        </table>
-                    </div>
+                    $p = (bool) TRUE;
+                    $q = (bool) FALSE;
+                    echo 'Valor verdadero: '.$p.'<br>';
+                    echo 'Valor falso: '.$q.'<br>';
+                    ?>
                 </div>
             </div>
         </div>
