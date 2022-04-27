0afce41dd1605a7eb1ae2dc8ce6d84ce3bef8cf6
Edenilson Pineda <edenilsonpineda@outlook.com>
2022-04-27 17:23:18 -0600

    fix html structure

--- lesson_2/fundamentals/strings.php
+++ lesson_2/fundamentals/strings.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="../../resources/css/bootstrap.min.css">
     <title>Lesson 2 - Fundamentals (Strings) </title>
 </head>
+
 <body>
     <main>
         <div id="custom-card" class="container px-4 py-5">
@@ -16,22 +18,22 @@
                     <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg">
                         <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
                             <?php
-                                /**
-                                 * @author Edenilson Pineda
-                                 * @version 1.0
-                                 * @see https://github.com/edenilsonpineda
-                                 * 
-                                 * Las variables flotantes son utilizadas para almacenar
-                                 * valores númericos con decimales, inclusive se puede
-                                 * utilizar el identificador de exponencial para un decimal 
-                                 * extenso.
-                                 */
+                            /**
+                             * @author Edenilson Pineda
+                             * @version 1.0
+                             * @see https://github.com/edenilsonpineda
+                             * 
+                             * Las variables flotantes son utilizadas para almacenar
+                             * valores númericos con decimales, inclusive se puede
+                             * utilizar el identificador de exponencial para un decimal 
+                             * extenso.
+                             */
 
 
-                                $firstName  = (String) 'Bill';
-                                $lastName   = (String) 'Gates';
-                                
-                                echo '<h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Resultado de concatenar dos cadenas de texto: <u>'.$firstName . '</u> ' . $lastName . '<h3>';
+                            $firstName  = (string) 'Bill';
+                            $lastName   = (string) 'Gates';
+
+                            echo '<h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Resultado de concatenar dos cadenas de texto: <u>' . $firstName . '</u> ' . $lastName . '</h3>';
                             ?>
                         </div>
                     </div>
