80c1aa6b440234810afaa55c400f4f2788c0f58d
Edenilson Pineda <edenilsonpineda@outlook.com>
2022-04-27 17:33:23 -0600

    add booleans example

---
+++ lesson_2/fundamentals/booleans.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../resources/css/bootstrap.min.css">
+    <title>Lesson 2 - Fundamentals (Strings) </title>
+</head>
+
+<body>
+    <main>
+        <div id="custom-card" class="container px-4 py-5">
+            <h2 class="pb-2 border-bottom">Lesson 2 - Fundamentals (Booleans) | Tabla de verdad </h2>
+            <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+                <div class="col">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-sm">
+                            <thead>
+                                <tr>
+                                    <th scope="col">p</th>
+                                    <th scope="col">q</th>
+                                    <th scope="col">P ^ Q</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                /**
+                                 * @author Edenilson Pineda
+                                 * @version 1.0
+                                 * @see https://github.com/edenilsonpineda
+                                 */
+
+                                $p = (boolean) True;
+                                $q = (boolean) False;
+                                $intersection = False;
+                                echo '<tr>';
+                                echo '<td>' . $p . '</td>';
+                                echo '<td>' . $q . '</td>';
+                                echo '<td>' . $intersection . '</td>';
+                                echo '</tr>';
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+<script src="../../resources/js/bootstrap.bundle.min.js"></script>
+
+</html>
