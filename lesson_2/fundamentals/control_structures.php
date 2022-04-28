0ac41de5d35884bb60ab42cc5876dcecaa68e621
Edenilson Pineda <edenilsonpineda@outlook.com>
2022-04-27 22:22:50 -0600

    add string functions examples

--- lesson_2/fundamentals/control_structures.php
+++ lesson_2/fundamentals/control_structures.php
@@ -13,7 +13,7 @@
     <main>
         <div id="custom-card" class="container px-4 py-5">
             <h2 class="pb-2 border-bottom">Lesson 2 - Fundamentals (Control Structures) </h2>
-            <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+            <div class="row row-cols-1 row-cols-lg-3 g-4 py-5">
                 <!-- Estructuras de control -->
                 <div class="col">
                     <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg">
