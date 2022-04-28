b0163a4f33707dd7a0dbc4c0f93d9a221e7163a4
Edenilson Pineda <edenilsonpineda@outlook.com>
2022-04-27 23:46:02 -0600

    add index with ref to lessons

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../resources/css/bootstrap.min.css">
+    <title>Lesson 2 - Fundamentals (Control Structures) </title>
+</head>
+
+<body>
+    <main>
+        <header>
+            <div class="px-3 py-2 bg-dark text-white">
+                <div class="container">
+                    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                        <div class="dropdown">
+                            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle show" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="true">
+                                Lesson 2
+                            </a>
+
+                            <ul class="dropdown-menu text-small show" aria-labelledby="dropdownUser1" data-popper-placement="top-start" data-popper-reference-hidden="">
+                                <li>
+                                    <a class="dropdown-item" href="lesson_2/fundamentals/arrays.php">Arrays [Arreglos]</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="lesson_2/fundamentals/booleans.php">Booleans [Booleanos]</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+                            <li>
+                                <a href="#" class="nav-link text-secondary">
+                                    <svg class="bi d-block mx-auto mb-1" width="24" height="24">
+                                        <use xlink:href="#home"></use>
+                                    </svg>
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link text-white">
+                                    <svg class="bi d-block mx-auto mb-1" width="24" height="24">
+                                        <use xlink:href="#speedometer2"></use>
+                                    </svg>
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link text-white">
+                                    <svg class="bi d-block mx-auto mb-1" width="24" height="24">
+                                        <use xlink:href="#table"></use>
+                                    </svg>
+                                    Orders
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link text-white">
+                                    <svg class="bi d-block mx-auto mb-1" width="24" height="24">
+                                        <use xlink:href="#grid"></use>
+                                    </svg>
+                                    Products
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link text-white">
+                                    <svg class="bi d-block mx-auto mb-1" width="24" height="24">
+                                        <use xlink:href="#people-circle"></use>
+                                    </svg>
+                                    Customers
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </main>
+</body>
+<script src="../../resources/js/bootstrap.bundle.min.js"></script>
+
+</html>
